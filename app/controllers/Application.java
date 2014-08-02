cd90e58483f2373e5092300847decab0954f370f
Allen Shi <shidenghui@gmail.com>
2014-08-02 18:49:58 +0200

    change the authentication with session

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,33 +1,171 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Email;
 import play.mvc.*;
 
-import java.util.*;
+import java.io.PrintWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Random;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Random;
 
-import models.*;
+import models.Image;
+import models.Post;
+import models.User;
+import play.data.validation.Email;
+import play.db.jpa.Blob;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
-	public static void index() {
-		render();
-	}
+	public static void register(@Email String email, String password,
+			String password_retyped) {
 
-	public static void register(String email, String password1, String password2) {
 		boolean register = true;
 		boolean flag_register = false;
 		boolean flag_twice = false;
-		if (password1.equals(password2)) {
-			if (User.find("byEmail", email).first() == null) {
-				flag_register = true;
-				new User(email, password1).save();
-				render("Application/index.html", flag_register, register);
+
+		String userId = session.get("userId");
+		System.out.println("###before register userID: " + userId);
+		if (userId != null) {
+			User user = User.findById(Long.parseLong(userId));
+			if (user != null) {
+				index();
+			}
+		}
+
+		User user = User.find("byEmailAndPassword", email, password).first();
+		if (user != null) {
+			flash.error("Your are already registered. Please go to Login.");
+			index();
+		}
+
+		if (validation.email(email).ok == false) {
+			flash.error("Invalid e-mail address. Please check the right spelling and try to register again.");
+			index();
+		}
+
+		if (user == null) {
+
+			if (password.equals(password_retyped) == false) {
+
+				flag_twice = true;
+				flash.error("Password mismatch. Please try to register again.");
+				// index();
+				render("Application/index.html", flag_twice, register);
+
 			} else {
-				render("Application/index.html", flag_register, register);
+				if (password.length() < 5) {
+					flash.error("Your password is too short. Please check if your password has the minimum size of 5 characters.");
+					index();
+				} else {
+
+					flag_register = true;
+					User newUser = new User(email, password);
+					newUser.save();
+					session.put("userId", newUser.id);
+					// index();
+					render("Application/index.html", flag_register, register);
+				}
+			}
+		}
+	}
+
+	public static void index() {
+		String userId = session.get("userId");
+
+		if (userId != null) {
+			User user = User.findById(Long.parseLong(userId));
+			if (user != null) {
+				List<Post> posts = user.posts;
+				List<Post> allPostsOrderedByLikes = Post.getAllOrderedByLikes();
+				List<Post> allPostsOrderedByTags = Post.findAll();
+				render(user, posts, allPostsOrderedByLikes,
+						allPostsOrderedByTags);
+			}
+		}
+
+		// if no (valid) user authenticated
+
+		Post mostLikedPost = Post.getMostLikedWithImages();
+		Post recentlyFinishedPost = Post.getMostRecentWithImages();
+
+		render(mostLikedPost, recentlyFinishedPost);
+	}
+
+	// Login with e-mail address and password.
+	public static void login(String email, String password) {
+
+		boolean flag_login = false;
+		boolean login = false;
+
+		 //if user already login
+		 String userId = session.get("userId");
+		 if (userId != null) {
+		 User user = User.findById(Long.parseLong(userId));
+		 if (user != null) {
+		 MyProfile.page();
+		 }
+		 }
+
+		User user = User.find("byEmail", email).first();
+
+		if (user != null) {
+			if (user.password.equals(password) == true) {
+				session.put("userId", user.id);
+				flag_login = true;
+				render("Application/index.html", flag_login, login, email);
+			} else {
+				flash.error("Password mismatch. Please try to login again.");
+				render("Application/index.html", flag_login, login, email);
 			}
 		} else {
-			flag_twice = true;
-			render("Application/index.html", flag_twice, register);
+			flash.error(email + " does not exist. Please go to Register.");
 		}
+
+		index();
+		render("Application/index.html", flag_login, login, email);
+
+	}
+
+	public static void logout() {
+
+		if (session.contains("googleAccessToken")) {
+			String token = session.get("googleAccessToken");
+			System.out.println("logout: Google access token :" + token);
+			try {
+				new URL("https://accounts.google.com/o/oauth2/revoke?token="
+						+ token).openConnection().getInputStream();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		// session.put("userId", null);
+		session.clear();
+		index();
+	}
+
+	public static void pageNotAuthenticated(int error) {
+		if (error == 1) {
+			renderArgs.put("passError", "true");
+		} else if (error == 2) {
+			renderArgs.put("userExists", "true");
+		} else if (error == 3) {
+			renderArgs.put("loginError", "true");
+		} else if (error == 4) {
+			renderArgs.put("emailError", "true");
+		}
+
+		renderArgs.put("authenticated", "false");
+
+		render();
 	}
 }
--- app/models/Post.java
+++ app/models/Post.java
@@ -6,6 +6,7 @@ package models;
 
 import java.util.Date;
 import java.util.List;
+
 import play.db.jpa.Model;
 
 import javax.persistence.CascadeType;
@@ -57,26 +58,69 @@ public class Post extends Model {
 		this.rating = 0.0;
 	}
 
-	public static Post getMostRecent() {
+	public static Post getMostRecent()
+	{
 		List<Post> posts = Post.findAll();
-
-		if (posts.size() > 0) {
+		
+		if(posts.size() > 0)
+		{
 			return posts.get(posts.size() - 1);
+		}
+		
+		return null;
+	}
+	
+	public static Post getMostRecentWithImages()
+	{
+		List<Post> posts = Post.findAll();
+		
+		for (int i = posts.size() - 1; i >= 0; i--) {
+			Post post = posts.get(i);
+			if (post.content.pictures.size() > 0) {
+				return post;
+			}
 		}
 
 		return null;
 	}
+	
 
-
-	public static Post getMostLiked() {
-
+	public static Post getMostLikedWithImages() {
+		
 		List<Post> chronologicalPosts = Post.findAll();
-
+		
 		if (chronologicalPosts.size() > 1) {
+
+			List<Post> posts = Post.find("order by rating desc").fetch();
+			Long mostRecentPostId = chronologicalPosts.get(chronologicalPosts.size() - 1).getId();
+
+			for (int i = 0; i < posts.size() - 1; i++) {
+				if (posts.get(i).content.pictures.size() > 0 && posts.get(i).getId().compareTo(mostRecentPostId) != 0) {
+					return posts.get(i);
+				}
+			}
+		}
+
+		return null;
+	}
+	
+	public static Post getMostLiked() {
+		
+		if (Post.findAll().size() > 1) {
 
 			List<Post> posts = Post.find("order by rating desc").fetch();
 
 			return posts.get(0);
+		}
+
+		return null;
+	}
+	
+	public static List<Post> getAllOrderedByLikes() {
+		
+		if (Post.findAll().size() > 1) {
+
+			return Post.find("order by rating desc").fetch();
 		}
 
 		return null;
