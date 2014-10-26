4d07b7d31a0109308526c18efb58ce9071cd9195
linxj <linxj92@gmail.com>
2014-10-26 15:32:27 +0100

    wrong password redirect

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,14 +3,13 @@ package controllers;
 import play.mvc.Controller;
 
 public class Application extends Controller {
-
 	/**
 	 * index
 	 */
-	public static void index(boolean promote_login) {
+	public static void index(boolean promote_login, boolean wrongPW) {
 		// see if login
 		String email = session.get("username");
-		System.out.println(session.get("userId"));
+		
 		if(email != null){
 			// has login
 			// redirect to home
@@ -20,11 +19,12 @@ public class Application extends Controller {
 			render(flag_login, login, email);
 		}else{
 			// not login
-			render(promote_login);
+			render(promote_login, wrongPW);
 		}
 	}
 	
 	public static void index() {
+		System.out.println("here");
 		// see if login
 		String email = session.get("username");
 		System.out.println(session.get("userId"));
