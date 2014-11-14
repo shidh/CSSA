9bf7b17bfd508af273f156f121506a74c008998e
Allen Shi <shidenghui@gmail.com>
2014-11-14 19:26:36 +0100

    enable user to see events withput login

--- app/controllers/EventStream.java
+++ app/controllers/EventStream.java
@@ -16,19 +16,21 @@ public class EventStream extends Controller{
 		
 		System.out.println("From EventStream#current username: "+username);
 		boolean flag_login = false;
+		// 'size' is the number of elements displayed per page
+	    // 'page' is the current page index, starting from 1.
+	    int start = (page-1) * size;
+		List<Event> posts = Event.find("order by postingDate desc").from(start).fetch(size);
+		
 		if (username != null)
 		{
 			//User user = User.findById(Long.parseLong(userId));
 			User user = User.find("byEmail", username).first();
+
+			
 			if (user != null)
 			{
 				flag_login = true;
 				String email = session.get("username");
-
-				// 'size' is the number of elements displayed per page
-			    // 'page' is the current page index, starting from 1.
-			    int start = (page-1) * size;
-				List<Event> posts = Event.find("order by postingDate desc").from(start).fetch(size);
 
 				//render(user, posts);
 				render(posts, flag_login, email, size, page);
@@ -36,7 +38,11 @@ public class EventStream extends Controller{
 
 	
 		}
-		boolean promote_login = true;
-		Application.index(promote_login, false);
+		//set true to ask user login firstly
+		//boolean promote_login = true;
+		//Application.index(promote_login, false);
+		render(posts, flag_login, size, page);
+
+
 	}
 }
